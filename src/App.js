5968089e68fb09d80fc4af586ec57543cfa01cef
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-07-09 19:43:16 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-    <h1>My Awesome Blog</h1>
-     <div id="page-body">
-      Welcome to my blog!
-     </div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Awesome Blog</h1>
+        <div id="page-body">
+          <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/article' element={<ArticlePage />} />
+          <Route path='/articleslist' element={<ArticlesListPage />} />
+          </Routes>
+         
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
